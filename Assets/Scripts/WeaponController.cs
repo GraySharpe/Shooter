06862db48046b41af06ecf91b5aab6956f84aad5
GraySharpe <grayson.sharpe@shaw.ca>
2015-05-05 18:52:16 -0700

    [please work]

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour 
+{
+	Weapon currentWeapon;
+
+
+	void Start () 
+	{
+		currentWeapon = GetComponentInChildren<Weapon> ();
+		Screen.lockCursor = true;
+	
+	}
+	
+
+	void Update () 
+	{
+		if(currentWeapon)
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				currentWeapon.Fire();
+			}
+		}
+	}
+}
